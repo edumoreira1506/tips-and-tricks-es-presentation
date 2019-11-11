9ddc725113ec4b44c14e40ae28e669a1377bcee6
edumoreira1506 <contato@eduardoem.com.br>
2019-11-11 09:59:11 -0300

    Updates

---
+++ precache-manifest.0b40ae27cdd7acdf0f960df1241bb7b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5bd5c61463d8b5a7c97aa69acc9fb2",
+    "url": "/tips-and-tricks/index.html"
+  },
+  {
+    "revision": "d396d231cb64bd88afe8",
+    "url": "/tips-and-tricks/static/css/main.325a5501.chunk.css"
+  },
+  {
+    "revision": "b40ce21b5cf8b0776bf1",
+    "url": "/tips-and-tricks/static/js/2.78a20a17.chunk.js"
+  },
+  {
+    "revision": "d396d231cb64bd88afe8",
+    "url": "/tips-and-tricks/static/js/main.0778a7cf.chunk.js"
+  },
+  {
+    "revision": "9a3b69841f28b3979148",
+    "url": "/tips-and-tricks/static/js/runtime-main.82901151.js"
+  }
+]);
